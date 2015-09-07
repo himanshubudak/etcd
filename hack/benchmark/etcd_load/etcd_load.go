c4b38dc4ca6a0130c88f9af0ed67333a7b3370ec
Himanshu Roy <hroy@dhcp35-180.lab.eng.blr.redhat.com>
2015-09-07 16:33:36 +0530

    etcd_load moved to a new directory

---
+++ hack/benchmark/etcd_load/etcd_load.go
@@ -0,0 +1,263 @@
+package main
+
+import (
+    "strconv"
+    "strings"
+    "os"
+    "fmt"
+    "github.com/coreos/go-etcd/etcd"
+    "crypto/rand"
+    "time"
+    "sync"
+    "os/exec"
+    "math/big"
+    "log"
+    "code.google.com/p/gcfg"
+)
+
+/*
+Declarations :::: 
+    actions : for passing otherfunctions as arguments to handler function
+    operation : which operation to perform
+    keycount : number of keys to be added, retrieved, deleted or updated
+    threads : number of total threads
+    pct : each entry represents percentage of values lying in (value_range[i],value_range[i+1]) 
+*/
+
+type actions func(int,int)
+
+var n int32
+var wg sync.WaitGroup
+var operation string
+var keycount int
+var operation_count int
+var threads int
+var client *etcd.Client
+var pct [5]int
+var pct_count [5]int
+var value_range [6]int
+var pidetcd string
+var start time.Time
+var f *os.File
+var err error
+
+
+func main() {
+
+    // Configuration Structure
+    cfg := struct {
+        Section_Args struct {
+            Etcdhost string
+            Etcdport string
+            Operation string
+            Keycount string
+            Operation_Count string
+            Log_File string
+        }
+        Section_Params struct {
+            Pct []string
+            Value_Range []string
+        }
+    }{}
+    
+    // Config File
+    filename := os.Args[1]
+
+    err := gcfg.ReadFileInto(&cfg, filename)
+    if err != nil {
+        log.Fatalf("Failed to parse gcfg data: %s", err)
+    }
+    
+    // Reading from config file
+    etcdhost := cfg.Section_Args.Etcdhost
+    etcdport := cfg.Section_Args.Etcdport
+    operation = cfg.Section_Args.Operation
+    keycount = int(toInt(cfg.Section_Args.Keycount,10,64))
+    operation_count = int(toInt(cfg.Section_Args.Operation_Count,10,64))
+    log_file := cfg.Section_Args.Log_File
+    
+    pidtemp, _ := exec.Command("pidof","etcd").Output()
+    pidetcd = string(pidtemp)
+    pidetcd = strings.TrimSpace(pidetcd)
+    etcdmem_s, _  := strconv.Atoi(getMemUse(pidetcd))
+    fmt.Println("This is the current memory usage by etcd before execution: " + getMemUse(pidetcd))
+
+    var machines = []string{"http://"+etcdhost+":"+etcdport}
+    client =  etcd.NewClient(machines)
+    
+    //Log File
+    f, err = os.OpenFile(log_file, os.O_RDWR | os.O_CREATE , 0666)
+    if err != nil {
+        log.Fatalf("error opening file: %v", err)
+    }
+    
+    log.SetOutput(f)
+    log.Println("Starting #####")
+    log.Println("Keycount = %s , operation_count = %s",cfg.Section_Args.Keycount,cfg.Section_Args.Operation_Count)
+
+    
+
+    
+
+
+
+
+    // Percentage distribution of key-values
+    pct_temp := [5]int{5, 74, 10, 10, 1}
+    pct = pct_temp
+    value_range_temp := [6]int{0, 256, 512, 1024, 8192, 204800}
+    value_range = value_range_temp
+
+    var pct_count [5] int
+    for i:=0;i<5;i++{
+        pct_count[i] = pct[i] * keycount / 100
+    }
+
+    // Number of threads
+    threads = 5
+
+    // Keep track of the goroutines
+    wg.Add(len(pct))
+    
+    switch{
+    case operation == "create":
+        log.Println("Operation : create")
+        var values [2]int
+        base := 0
+        for i:=0;i<len(pct);i++{
+            values[0] = value_range[i]
+            values[1] = value_range[i+1]
+            go create_keys(base,pct_count[i],values)
+        }
+        wg.Wait()
+    case operation == "get":
+        log.Println("Operation : get")
+        handler(get_values)
+        wg.Wait()
+    case operation == "update":
+        log.Println("Operation : update")
+        handler(update_values)
+        wg.Wait()
+    case operation == "delete":
+        log.Println("Operation : delete")
+        handler(delete_values)
+        wg.Wait()
+    }
+    etcdmem_e, _ := strconv.Atoi(getMemUse(pidetcd))
+    fmt.Println("This is the current memory usage by etcd after execution: " + getMemUse(pidetcd))
+    fmt.Println("Difference := " + strconv.Itoa(etcdmem_e - etcdmem_s))
+    log.Println("Difference in memory use, after and before load testing := " + strconv.Itoa(etcdmem_e - etcdmem_s))
+    defer f.Close()
+}
+
+func toInt(s string, base int, bitSize int) int64 {
+    i, err := strconv.ParseInt(s, base, bitSize)
+    if err != nil {
+        panic(err)
+    }
+    return i
+}
+
+func toString(i int) string {
+    s := strconv.Itoa(i)
+    return s
+}
+
+func get_values(base int, per_thread int){
+    var key int
+    limit := base + (keycount / threads)
+    fmt.Println("limit is = ",strconv.Itoa(limit))
+    for i:=0;i<per_thread;i++{
+        m,_ := rand.Int(rand.Reader,big.NewInt(int64(limit-base)))
+        key = int(m.Int64()) + base
+        start = time.Now()
+        result, _ := client.Get(toString(key),false,false)
+        elapsed := time.Since(start)
+        log.Println("key %s took %s", key, elapsed)
+        fmt.Println(result)
+    } 
+    defer wg.Done()
+}
+
+func create_keys(base int, count int, r [2]int){
+    var key int
+    for i:=0;i<count;i++{
+        key = base + i
+        m,_ := rand.Int(rand.Reader,big.NewInt(int64(r[1]-r[0])))
+        r1 := int(m.Int64()) + r[0]
+        value := RandStringBytesRmndr(r1)
+        start = time.Now()
+        result, _ := client.Set(toString(key),value,1000)
+        elapsed := time.Since(start)
+        log.Println("key %s took %s", key, elapsed)
+        _ = result
+    }
+    defer wg.Done()
+}
+
+func update_values(base int, per_thread int){
+    var key int
+    val := "UpdatedValue"
+    limit := base + (keycount / threads)
+    for i:=0;i<per_thread;i++{
+        m,_ := rand.Int(rand.Reader,big.NewInt(int64(limit-base)))
+        key = int(m.Int64()) + base
+        start = time.Now()
+        result, _ := client.Set(toString(key),val,1000) 
+        elapsed := time.Since(start)
+        log.Println("key %s took %s", key, elapsed)
+        fmt.Println(result)
+    }
+    defer wg.Done()
+}
+
+func delete_values(base int, per_thread int){
+    var key int
+    limit := base + (keycount / threads)
+    for i:=0;i<per_thread;i++{
+        m,_ := rand.Int(rand.Reader,big.NewInt(int64(limit-base)))
+        key = int(m.Int64()) + base
+        start = time.Now()
+        result, _ := client.Delete(toString(key),false)
+        elapsed := time.Since(start)
+        log.Println("key %s took %s", key, elapsed)
+        fmt.Println(result)
+    }
+    defer wg.Done()
+}
+
+
+func handler(fn actions){
+    per_thread := operation_count/threads
+    base := 0
+    for i:=0;i<threads;i++{
+        go fn(base,per_thread)
+        base = base + (keycount/threads)
+    }
+}
+
+func RandStringBytesRmndr(n int) string {
+    const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    b := make([]byte, n)
+    for i := range b {
+        m,_ := rand.Int(rand.Reader,big.NewInt(int64(len(letterBytes))))
+        b[i] = letterBytes[int(m.Int64())]
+    }
+    return string(b)
+}
+
+func getMemUse(pid string) string{
+    mem, _ := exec.Command("pmap","-x",pid).Output()
+    mempmap := string(mem)
+    temp := strings.Split(mempmap,"\n")
+    temp2 := temp[len(temp)-2]
+    temp3 := strings.Fields(temp2)
+    memory := temp3[3]
+    return memory
+}
+
+func timeTrack(start time.Time, name string) {
+    elapsed := time.Since(start)
+    log.Println("key %s took %s", name, elapsed)
+}
+
